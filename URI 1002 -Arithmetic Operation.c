c75115abf3da47df45c4f404491c0de29f4519c4
ArnobMahmud <arnob.tech.me@gmail.com>
2020-03-28 16:25:17 +0600

    URI 1002

---
+++ URI 1002 -Arithmetic Operation.c
@@ -0,0 +1,64 @@
+﻿/*
+  Author : Arnob Mahmud
+  Mail : arnob.tech.me @ gmail.com
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) {
+
+    float a, b, sum, x, y, z;
+
+    printf("Enter the value of a(Fractional Number):\n");
+    scanf("%f", &a );
+
+    printf("Enter the value of b(Fractional Number):\n");
+    scanf("%f", &b );
+
+    sum = a + b;
+
+    printf("The value of sum is:%f\n", sum);    //////// This output will show the value of sum upto 6 digits after the point(.)/////////////
+    printf("The value of sum is:%.2f\n", sum);   ///This time this will show 2 digits after the point(.)//  ///  .2f commands for the 2 digit after point number ///
+
+    printf("Enter the value of a(Fractional Number):\n");
+    scanf("%f", &a );
+
+    printf("Enter the value of b(Fractional Number):\n");
+    scanf("%f", &b );
+
+    sum = a - b;
+
+    printf("The value of sum is:%f\n", sum);
+    printf("The value of sum is:%.2f\n", sum);
+
+
+    printf("Enter the value of x:\n");
+    scanf("%f", &x );
+
+
+    printf("Enter the value of y:\n");
+    scanf("%f", &y );
+
+
+    z = x * y;
+
+    printf("The value of z is:%f\n", z);
+    printf("The value of sum is:%.2f\n", sum);
+
+    printf("Enter the value of x:\n");
+    scanf("%f", &x );
+
+    printf("Enter the value of y:\n");
+    scanf("%f", &y );
+
+    z = x / y;
+
+    printf("The value of z is:%f\n", z);
+    printf("The value of sum is:%.2f\n", sum);
+
+    return 0;{
+      
+    }
+
+}
