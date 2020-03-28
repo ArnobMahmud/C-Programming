b7c78395f01aed2621c8fb9e5f230fdcbda5ad18
ArnobMahmud <arnob.tech.me@gmail.com>
2020-03-28 16:36:54 +0600

    URI 1001

---
+++ URI 1001 -Hello World.c
@@ -0,0 +1,14 @@
+/* 
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me@gmail.com
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello World!\n");
+
+	return 0;
+}
