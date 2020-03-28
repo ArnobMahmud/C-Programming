1e7eaca2371aed9cc9b8147d653ec69c8c4fd0e8
ArnobMahmud <arnob.tech.me@gmail.com>
2020-03-29 05:54:53 +0600

    URI 1011

---
+++ URI 1011 -Switch Case.c
@@ -0,0 +1,47 @@
+﻿/*
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me @ gmail.com
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    int CGPA;
+
+    printf("Enter your CGPA:");
+    scanf("%d", &CGPA);
+
+    switch (CGPA) {
+
+    case 0 :
+    printf("Fail");
+    break;
+
+    case 1:
+    printf("Poor");
+    break;
+
+    case 2:
+    printf("Average");
+    break;
+
+    case 3:
+    printf("Good");
+    break;
+
+    case 4:
+    printf("Excellent");
+    break;
+
+    default :
+    printf("Illegal CGPA");
+    break;
+
+    }
+    
+    return 0;
+}
