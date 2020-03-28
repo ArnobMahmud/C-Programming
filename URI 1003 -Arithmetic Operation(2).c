afb8ac5892c00855cb852fe05895894760434305
ArnobMahmud <arnob.tech.me@gmail.com>
2020-03-28 16:45:57 +0600

    URI 1003

---
+++ URI 1003 -Arithmetic Operation(2).c
@@ -0,0 +1,23 @@
+/*
+  Author : Arnob Mahmud
+  Mail : arnob.tech.me @ gmail.com
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[]) {
+
+  float ft_marks, st_marks, final_marks;
+  short total_marks;
+
+  ft_marks = 80;
+  st_marks = 74;
+  final_marks = 97;
+
+  total_marks = ft_marks / 4 + st_marks / 4 + final_marks / 2;
+  printf("Total mark is :%ld\n", total_marks);
+
+  return 0;
+
+}
