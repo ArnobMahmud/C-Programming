d28382584a993039599ed452d8f9bf73d82afe74
ArnobMahmud <arnob.tech.me@gmail.com>
2020-03-29 01:39:39 +0600

    URI 1006

---
+++ URI 1006 -Algorithm(2).c
@@ -0,0 +1,30 @@
+﻿/*
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me @ gmail.com
+*/
+
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(int argc, char const *argv[])
+{ 
+
+   float h, b, area;
+
+   printf("Enter the value of base:");
+   scanf("%f", &b);
+
+
+   printf("Enter the value of height:");
+   scanf("%f", &h);
+
+
+   area = .5 * b * h;
+
+  printf("The area of triangle is:%.2f", area);
+  
+  return 0;
+
+}
