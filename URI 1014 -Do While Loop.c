d6b1b8769af378ff7923058d2c3b01655623a1c7
ArnobMahmud <arnob.tech.me@gmail.com>
2020-03-29 06:32:54 +0600

    URI 1014

---
+++ URI 1014 -Do While Loop.c
@@ -0,0 +1,30 @@
+﻿/*
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me @ gmail.com
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+  
+  int i;
+  i = 10;
+
+    do {
+
+        printf("*****\n");
+        printf("****\n");
+        printf("***\n");
+        printf("**\n");
+        printf("*\n");
+
+        i--;
+
+    } while ( i >= -2);
+
+  return 0;
+
+}
