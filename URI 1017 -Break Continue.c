6fbbfcfc86d5f097163b84c653c39aa2cc41e676
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-03-30 11:50:47 +0600

    C 1017

---
+++ URI 1017 -Break Continue.c
@@ -0,0 +1,53 @@
+﻿/*
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me@gmail.com
+
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+int main () {
+
+    int i;
+
+    printf (" Only odd number will be printed [For Loop] :\n");
+
+    for (i = 1; i <= 50; i++) {
+
+        if ( i % 2 == 0) {
+        continue;
+        }
+
+        printf (" %d\n", i);
+
+    }
+
+    printf ("Value [For Loop] :\n");
+    
+    for (i = 1; i <= 50; i++) {
+
+        if ( i % 3 == 0 ) {
+        continue;
+        }
+
+        printf (" %d\n", i);
+
+    }
+
+    printf ("Product [For Loop]:\n");
+
+    for (i = 1; i <= 50; i++) {
+
+        if ( i % 10 == 0 ) {
+        break;  
+        }
+
+        printf (" %d\n", i);
+
+    }
+
+  return 0;
+
+}
