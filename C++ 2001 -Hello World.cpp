a573d3aec9aab849a4fa3315fb04aac966992a9c
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-03-30 12:43:15 +0600

    C++ 2001

---
+++ C++ 2001 -Hello World.cpp
@@ -0,0 +1,17 @@
+/*
+  Author : Arnob Mahmud
+  
+  Mail : arnob.tech.me@gmail.com
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+	cout << "Hello World!!\n";
+	cout << "Welcome to C++.\n";
+
+	return 0;
+}
