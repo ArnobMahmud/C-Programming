ea67be346e9372bf76411dacacf2fe8c79749572
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-03-30 11:40:42 +0600

    C 1009

---
+++ C 1009 -Leap Year.c
@@ -0,0 +1,30 @@
+﻿/*
+  Author : Arnob Mahmud
+
+  Mail : arnob.tech.me @ gmail.com
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int year;
+
+    printf("Enter a year:");
+    scanf("%d", &year);
+
+    if (year % 4 == 0) {                          
+
+        printf("This is a leap year.");
+    } else {
+
+        printf("This is not a leap year.");
+    }
+
+    getch();
+
+	return 0;
+
+}
