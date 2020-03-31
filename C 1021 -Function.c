baf6e0dfa655383a41ed31cd427f89060b10b592
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-03-31 13:58:43 +0600

    C 1021

---
+++ C 1021 -Function.c
@@ -0,0 +1,29 @@
+/*
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me@gmail.com
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+ double avg(double a, double b){
+
+ return (a + b) / 2;
+}
+
+int main(int argc, char const *argv[])
+{
+  double x, y, z;
+
+  printf("Enter 3 numbers :\n");
+  scanf("%lf %lf %lf", &x, &y, &z);
+
+  printf("Average of %g and %g :%g\n", x, y, avg(x,y));
+  printf("Average of %g and %g :%g\n", y, z, avg(y,z));
+  printf("Average of %g and %g :%g\n", z, x, avg(z,x));
+
+  getch();
+  return 0;
+
+}
