5a06bcdba65b75877f1078adbf97cdab74ffd173
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-04-03 14:57:21 +0600

    C++ 2018 Update

--- C++ 2018 -GCD LCM.cpp
+++ C++ 2018 -GCD LCM.cpp
@@ -18,15 +18,15 @@ int main(int argc, char const *argv[]) {
   std::cin >> num2;
 
   while ( num2 != 0)
-				{
-					rem = num1 % num2;
+	{
+	   rem = num1 % num2;
 
-					num1 = num2;
-					num2 = rem;
-				}
+	   num1 = num2;
+	   num2 = rem;
+	}
 
-				GCD = num1;
-				LCM = num1 * num2 / GCD;
+	GCD = num1;
+	LCM = num1 * num2 / GCD;
 
    std::cout << "GCD is :" << GCD << '\n';
    std::cout << "LCM is :" << LCM << '\n';
