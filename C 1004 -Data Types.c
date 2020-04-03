54e045e417feedb8af49f674fc56d14d73c3f991
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-04-03 16:22:41 +0600

    C 1004 Update

--- C 1004 -Data Types.c
+++ C 1004 -Data Types.c
@@ -10,30 +10,36 @@ int main(int argc, char const *argv[])
 {
   
   int a, b, c; 
-  char s;   
-
+  float m;
+  char ch;
+  double x;
+  
+  
     printf("Enter the value of a:\n");
     scanf("%d", &a);
-
     printf("The value of a is: %d\n", a);
 
     printf("Enter the value of b:\n");
     scanf("%d", &b);
-
     printf("The value of b is: %d\n", b);
 
     printf("Enter the value of c:\n");
     scanf("%d", &c);
-
     printf("The value of c is: %d\n", c);
-
-
+     
+    printf("Enter the value of m:\n");
+    scanf("%f", &m);
+    printf("The value of c is: %f\n", m);
+     
     printf("Enter a string :\n");
-    scanf("%s", &s);
-
-    printf("Your string is :\n", s);
-
+    scanf("%c", &ch);
+    printf("Your string is :\n", ch);
+     
+    printf("Enter the value of x:\n");
+    scanf("%lf", &x);
+    printf("The value of c is: %f\n", x);
+     
+    
   return 0;
 
 }
-
