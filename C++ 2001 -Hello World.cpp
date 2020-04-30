d0af7300f4fb421d0aeee46cb5cf2efd0b0885b2
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-04-30 20:09:38 +0600

    C++ 2001

--- C++ 2001 -Hello World.cpp
+++ C++ 2001 -Hello World.cpp
@@ -4,14 +4,14 @@
   Mail : arnob.tech.me@gmail.com
 */
 
-#include <iostream>
+#include <bits/stdc++>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
 
 	cout << "Hello World!!\n";
-	cout << "Welcome to C++.\n";
+	cout << "Welcome to C++ Section.\n";
 
 	return 0;
 }
