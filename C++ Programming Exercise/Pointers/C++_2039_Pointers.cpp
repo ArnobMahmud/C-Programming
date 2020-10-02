bcf77026c9e3067ed97bca1d2fd817558c4c46fd
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-02 11:39:29 +0600

    Pointers in C++

---
+++ C++ Programming Exercise/Pointers/C++_2039_Pointers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x = 10;
+    int *p;
+    p = &x;
+
+    cout << x << endl;
+    cout << &x << endl;
+    cout << p << endl;
+    cout << *p << endl;
+
+    return 0;
+}
+
